01e4ac82d169d5153e27fe479e8321370f137d42
Vedrana <vedrana_ravlic16@yahoo.com>
2018-01-30 21:42:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,22 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  
+  var x = [];
+  var y = 'In your cart, you have';
+  for (var i = 0; i < cart.length; i++){
+    var itemName = Object.keys(cart[i]);
+    var priceNum = cart[i][itemName];
+        x.push(`${itemName} at $ ${priceNum}`);
+    }
+  var middle = x.join(' and ');
+  var beginning = x.slice(0, -1).join(', ');
+  var end = x.slice(-1);
+
+  if (cart.length === 0){console.log('Your shopping cart is empty.')}
+  else if (cart.length === 1){console.log(`${y} ${x}.`)}
+  else if (cart.length === 2){console.log(`${y} ${middle}.`)}
+  else if (cart.length > 2){return(`${y} ${beginning}, and ${end}.`)}
+  }
 }
 
 function total() {
