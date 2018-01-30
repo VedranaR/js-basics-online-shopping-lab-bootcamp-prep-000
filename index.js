36e77018df60c2550a27993043bc4bdab581b2ec
Vedrana <vedrana_ravlic16@yahoo.com>
2018-01-30 20:40:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ function setCart(c) {
 function addToCart(item) {
   const price = (Math.floor(Math.random() * 100+1));
   cart.push({[item]: price});
-}
   console.log(`${item} has been added to your cart.`);
   return cart;
 }
